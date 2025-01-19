15dbca1e17282f48d966769afd54c4aead261457
crystaller347 <149484630+crystaller347@users.noreply.github.com>
2025-01-19 17:19:40 +0000

    Deploying to gh-pages from @ Volodymyr2121/EvoPlay@c7d90c346cda762df1bb3f17dc9e85cc429767eb 🚀

---
+++ assets/privacy-82904e14.js
@@ -0,0 +1,2 @@
+const c=document.querySelector(".privacy-burger"),t=document.querySelector(".close-menu-but"),e=document.querySelector(".mobile-nav");c.addEventListener("click",()=>{e.classList.remove("hidden")});t.addEventListener("click",()=>{e.classList.add("hidden")});
+//# sourceMappingURL=privacy-82904e14.js.map
--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/styles-3d025ce0.js";const t=document.querySelector(".open-menu-but"),n=document.querySelector(".close-menu-but"),e=document.querySelector(".mobile-nav");t.addEventListener("click",()=>{e.classList.remove("hidden")});n.addEventListener("click",()=>{e.classList.add("hidden")});
+import"./assets/styles-ee39cd90.js";const t=document.querySelector(".open-menu-but"),n=document.querySelector(".close-menu-but"),e=document.querySelector(".mobile-nav");t.addEventListener("click",()=>{e.classList.remove("hidden")});n.addEventListener("click",()=>{e.classList.add("hidden")});
 //# sourceMappingURL=commonHelpers.js.map
