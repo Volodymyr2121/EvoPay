d02ed6fab3c0e839f165732c6a25ebe0c3c800fe
crystaller347 <149484630+crystaller347@users.noreply.github.com>
2025-01-19 16:08:09 +0000

    Deploying to gh-pages from @ Volodymyr2121/EvoPlay@64606eedb002803cbd2248358306452a3c1c27a6 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-3d025ce0.js";const c=document.querySelector(".privacy-burger"),t=document.querySelector(".close-menu-but"),e=document.querySelector(".mobile-nav");c.addEventListener("click",()=>{e.classList.remove("hidden")});t.addEventListener("click",()=>{e.classList.add("hidden")});
+//# sourceMappingURL=commonHelpers2.js.map
