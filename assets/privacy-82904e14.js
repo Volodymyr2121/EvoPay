026f81333521615e01dba8e29597dc31c3fb1a22
crystaller347 <149484630+crystaller347@users.noreply.github.com>
2025-01-20 17:35:40 +0000

    Deploying to gh-pages from @ Volodymyr2121/EvoPlay@e2341c248429e6ef7a767e710c48583da6ad307a 🚀

--- assets/privacy-82904e14.js
+++
@@ -1,2 +0,0 @@
-const c=document.querySelector(".privacy-burger"),t=document.querySelector(".close-menu-but"),e=document.querySelector(".mobile-nav");c.addEventListener("click",()=>{e.classList.remove("hidden")});t.addEventListener("click",()=>{e.classList.add("hidden")});
-//# sourceMappingURL=privacy-82904e14.js.map
