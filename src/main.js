3231cfeb185a4a47e08d796dd6f8e0b4f755397a
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2025-01-22 00:37:00 +0100

    about the game

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//Mobile Modal
+import Swiper from 'swiper/bundle';
 
 const openMenuBut = document.querySelector('.open-menu-but');
 const closeMenuBut = document.querySelector('.close-menu-but');
@@ -10,4 +10,59 @@ openMenuBut.addEventListener('click', () => {
 
 closeMenuBut.addEventListener('click', () => {
   mobileNav.classList.add('hidden');
+});
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const swiper = new Swiper('.swiper', {
+//     loop: true, // для безкінечного циклу
+//     navigation: {
+//       nextEl: '.slider-button.next',
+//       prevEl: '.slider-button.prev',
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//   });
+
+//   // Додаткові обробники подій для кнопок навігації та пагінації
+//   swiper.on('slideChange', function () {
+//     const nextButton = document.querySelector('.slider-button.next');
+//     const prevButton = document.querySelector('.slider-button.prev');
+
+//     // Оновлення пагінації
+//     const paginationBullets = document.querySelectorAll(
+//       '.swiper-pagination-bullet'
+//     );
+//     paginationBullets.forEach(bullet =>
+//       bullet.classList.remove('swiper-pagination-bullet-active')
+//     );
+//     paginationBullets[swiper.realIndex].classList.add(
+//       'swiper-pagination-bullet-active'
+//     );
+//   });
+
+//   // Ініціалізація для початкового стану пагінації
+//   const paginationBullets = document.querySelectorAll(
+//     '.swiper-pagination-bullet'
+//   );
+//   if (paginationBullets.length > 0) {
+//     paginationBullets[swiper.realIndex].classList.add(
+//       'swiper-pagination-bullet-active'
+//     );
+//   }
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper', {
+    loop: true, // Для безкінечного циклу
+    navigation: {
+      nextEl: '.slider-button.next',
+      prevEl: '.slider-button.prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 });
