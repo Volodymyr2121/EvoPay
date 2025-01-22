cca24cc166e7df4f1f4b0e2b5b90caf7a711d19b
crystaller347 <qwertymum04@gmail.com>
2025-01-22 17:15:41 +0200

    start making gallery and unfinished why us section

---
+++ src/js/gallery/gallery.js
@@ -0,0 +1,45 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+// const swiper = new Swiper('.gallery-swiper-mobile', {
+//   slidesPerView: 1,
+//   autoplay: {
+//     delay: 2000,
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'bullets',
+//     clickable: 'true',
+//   },
+// });
+
+const bullets = document.querySelectorAll('.custom-bullet');
+
+const swiper = new Swiper('.gallery-swiper-mobile', {
+  slidesPerView: 1,
+  loop: true,
+  on: {
+    slideChange: function () {
+      updatePagination(swiper.realIndex); // Real index accounts for looping
+    },
+  },
+});
+
+function updatePagination(activeIndex) {
+  bullets.forEach((bullet, index) => {
+    if (index === activeIndex) {
+      bullet.classList.add('active');
+    } else {
+      bullet.classList.remove('active');
+    }
+  });
+}
+
+// const swiperDesktop = new Swiper('.swiper', {
+//   loop: true,
+//   slidesPerView: 1,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
