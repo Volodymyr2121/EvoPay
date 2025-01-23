40d57e3fd5f8e001c17aa92463fabfc765c52ec4
crystaller347 <qwertymum04@gmail.com>
2025-01-23 18:14:53 +0200

    style reviews mobile and partially fix gallery desktop

--- src/js/gallery/gallery.js
+++ src/js/gallery/gallery.js
@@ -3,12 +3,16 @@ export function initializeGallerySlider() {
   const indicators = document.querySelectorAll('.indicatorr');
   const prevButtons = document.querySelectorAll('.prevv');
   const nextButtons = document.querySelectorAll('.nextt');
+  const galleryItems = document.querySelectorAll('.gallery-li');
   let currentIndex = 4;
 
   const updateSlider = index => {
     galleryList.style.transform = `translateX(-${index * 100}%)`;
     indicators.forEach((indicator, i) => {
       indicator.classList.toggle('gallery-active', i === index);
+    });
+    galleryItems.forEach((item, i) => {
+      item.classList.toggle('gallery-image-active', i === index);
     });
   };
 
