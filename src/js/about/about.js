4002646f47a4efe87a2201595a03716e16bf9e1a
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2025-01-28 22:46:26 +0100

    finish

--- src/js/about/about.js
+++ src/js/about/about.js
@@ -16,16 +16,6 @@ export function initializeAboutSlider() {
     });
     slides[index].style.display = 'block';
     indicators[index].classList.add('active');
-
-    if (window.innerWidth >= 1200) {
-      if (index === 0) {
-        sliderImages[index].src = '../../img/about-the-game/lets-order.jpg';
-      } else if (index === 1) {
-        sliderImages[index].src = '../../img/about-the-game/welcome-dsk.jpg';
-      } else if (index === 2) {
-        sliderImages[index].src = '../../img/about-the-game/match.jpg';
-      }
-    }
   }
 
   showSlide(currentSlide);
